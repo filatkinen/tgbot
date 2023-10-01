d8fa5e88bd5452e576790a161d0e470538c3a0d8
filatkinen <filatkinen@gmail.com>
2023-10-01 16:48:43 +0300

    Added products

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/filatkinen/tgbot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -30,18 +32,61 @@ func main() {
 
 	updates := bot.GetUpdatesChan(u)
 
+	productService := product.NewService()
+
+	commander := NewCommander(bot)
+
 	for update := range updates {
 		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, " You wrote: "+update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			message, err := bot.Send(msg)
-			if err != nil {
-				log.Printf("got error while sending message: %s", err)
+			switch update.Message.Command() {
+			case "help":
+				commander.HelpCommand(update.Message)
+			case "list":
+				commander.ListCommand(update.Message, productService)
+			default:
+				commander.DefaultBehavior(update.Message)
 			}
-			log.Printf("\n message:%+v", message)
 		}
+	}
+}
+
+type Commander struct {
+	bot *tgbotapi.BotAPI
+}
+
+func NewCommander(bot *tgbotapi.BotAPI) *Commander {
+	return &Commander{bot: bot}
+}
+func (c *Commander) HelpCommand(message *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(message.Chat.ID,
+		"/help - help\n"+
+			"/list - list products")
+	_, err := c.bot.Send(msg)
+	if err != nil {
+		log.Printf("got error while sending message: %s", err)
+	}
+}
+
+func (c *Commander) ListCommand(message *tgbotapi.Message, productService *product.Service) {
+	pr := strings.Builder{}
+	pr.WriteString("Here all the products:\n\n")
+	for _, v := range productService.List() {
+		pr.WriteString(v.Title)
+		pr.WriteString("\n")
+	}
+	msg := tgbotapi.NewMessage(message.Chat.ID, pr.String())
+	_, err := c.bot.Send(msg)
+	if err != nil {
+		log.Printf("got error while sending message: %s", err)
+	}
+}
+
+func (c *Commander) DefaultBehavior(message *tgbotapi.Message) {
+	//log.Printf("[%s] %s", message.From.UserName, message.Text)
+	msg := tgbotapi.NewMessage(message.Chat.ID, "You wrote:"+message.Text)
+	_, err := c.bot.Send(msg)
+	if err != nil {
+		log.Printf("got error while sending message: %s", err)
 	}
 }
