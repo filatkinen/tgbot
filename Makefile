0979f6ab8869cc0dc8a16f0c0e2308bdb14df34c
filatkinen <filatkinen@gmail.com>
2023-10-01 13:25:34 +0300

    added bot

---
+++ Makefile
@@ -0,0 +1,7 @@
+BIN:="./bin/tgbot"
+
+build:
+	go build -v -o $(BIN) ./cmd/bot
+
+run: build
+	$(BIN)
--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"os"
+)
 
 func main() {
+	apiKey := os.Getenv("BOTAPIKEY")
+	if apiKey == "" {
+		log.Printf("env BOTAPIKEY is not set")
+		return
+	}
 
-	fmt.Println("Hello from main")
+	bot, err := tgbotapi.NewBotAPI(apiKey)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, " You wtite: "+update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
